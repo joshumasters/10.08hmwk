06ddd1bae7e2e6c327666bace07d5898d2f46078
Joshua Masters <joshuamasters724@Joshuas-MacBook-Pro.local>
2020-10-11 16:34:20 -0400

    Homework for 10.08

---
+++ Hmwk.java
@@ -0,0 +1,30 @@
+/**
+ * 10.08hmwk
+ */
+public class Hmwk {
+    
+    // Question 1 = 20
+
+
+    // Question 2
+    public static void main(String[] args) {
+        // char[] stars = new char[5];
+
+        // for (int i = 0; i < 5; i++) {
+            
+        //     stars[i] = '*';
+        //     System.out.println(stars);
+
+            
+        // }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print('*');
+            }
+         System.out.println();
+        }
+
+        
+}
+}
